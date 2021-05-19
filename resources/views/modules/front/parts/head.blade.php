f17bd89c41c7b5b36bdbd234e377492ef78abfe3
unknown_haribda <bekza.bekmuratov@gmail.com>
2021-05-19 17:25:07 +0600

    Lesson passings added;

--- resources/views/modules/front/parts/head.blade.php
+++ resources/views/modules/front/parts/head.blade.php
@@ -3,6 +3,7 @@
 <title>Simple</title>
 <link rel="shortcut icon" href="{{asset('favicon.svg')}}">
 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
 
 
